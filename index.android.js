df9b8fa4ab9084f898e11291b7b602b7d7d965cc
Emmanuel Pacheco <eeph31@gmail.com>
2017-05-06 00:29:57 -0500

    Se crea repositorio

---
+++ index.android.js
@@ -0,0 +1,6 @@
+/**
+ * TaxiManagment React Native App
+ */
+
+/**Le decimos al archivo de Android que inicie con el archivo de iOS al final es el mismo codigo para los dos*/
+require('./index.ios.js');
